ab710320914749ecff3c9f6b79119ed4526d7808
Marco <domandominguez@hotmail.com>
2020-01-16 18:12:44 -0800

    Update addTwoInt.java

--- addTwoInt/addTwoInt.java
+++ addTwoInt/addTwoInt.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class AddTwoInt{
-    
+    //add function
     public static int addTwoInt(int a, int b)
     {
         return a + b;
     }
 
+    //driver
     public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
 
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        System.out.println(addTwoInt(a,b));
     }
 }
