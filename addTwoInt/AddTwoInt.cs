f30e1a27fddb589399a2af612911844d4a40fd6e
Marco <domandominguez@hotmail.com>
2020-01-17 17:32:51 -0800

    Update AddTwoInt.cs

--- addTwoInt/AddTwoInt.cs
+++ addTwoInt/AddTwoInt.cs
@@ -1,4 +1,13 @@
-int AddTwoInt(int a, int b)
-{
-    return a + b;
+using System;
+class AddTwoInt{
+    public static int AddTwoInt(int a, int b){
+        return a + b;
+    }
+
+    public static main(string []args){
+        int a = Console.Read();
+        int b = Console.Read();
+
+        Console.WriteLine(AddTwoIntInt(a,b));
+    }
 }
